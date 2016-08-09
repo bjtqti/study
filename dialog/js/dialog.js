80b038dc1f273a9c9cc6b40522efa78916b484d3
ouyangli <278500368@qq.com>
2016-08-09 17:13:14 +0800

    update dialog

--- dialog/js/dialog.js
+++ dialog/js/dialog.js
@@ -1,43 +1,70 @@
+/**
+ * 对话框
+ * @param message 消息内容
+ * @param [options] 可选的配置信息
+ */
 
-
-function Dialog(options){
-	var config = {
-		element:null,
-    	onConfrim:function(){},
-    	onCancel:function(){}
-	};
-	for(var k in config){
-		if(config.hasOwnProperty(k)){
-			this[k] = options[k]!==undefined?options[k]:config[k];
+function Dialog(message,options){
+	var defaultConfig = this.config,
+		options = options || {};
+	for(var i in defaultConfig){
+		if(options[i]!== undefined){
+			this[i] = options[i];
+		}else{
+			this[i] = defaultConfig[i];
 		}
 	}
-	if(!options.element){
-		return false;
-	}
+	this.createBox(message);
 	this.bindEvent();
 }
 
 Dialog.prototype.bindEvent = function(){
-	var it = this,
-	    buttons = this.element.querySelectorAll('button');
-	if(buttons&&buttons[0]){
-		buttons[0].addEventListener('click',function(e){
-			it.onCancel();
-			it.hide();
-		});
+	this.container.addEventListener('click',this,false);
+}
+
+Dialog.prototype.handleEvent = function(e){
+	var button = e.target.innerHTML;
+	if(button ===this.cancelText){
+		this.onCancel();
+		return false;
 	}
-	if(buttons&&buttons[1]){
-		buttons[1].addEventListener('click',function(e){
-			it.onConfrim();
-			it.hide();
-		});
+	if(button===this.confirmText){
+		this.onConfrim();
+		return false;
 	}
 }
 
 Dialog.prototype.show = function(){
-	this.element.classList.add('active');
+	this.container.classList.add('active');
 }
 
 Dialog.prototype.hide = function(){
-	this.element.classList.remove('active');
+	this.container.classList.remove('active');
+}
+
+Dialog.prototype.config = {
+	active:false,
+    title:'提示',
+    confirmText:"确定",
+    cancelText:"取消",
+    onlyConfirm:false,
+    onConfrim:function(){},
+    onCancel:function(){}
+}
+
+Dialog.prototype.createBox = function(message){
+	var container = document.createElement('div');
+	container.setAttribute('class','dialog-container');
+	var html = '<div class="dialog">'+
+					'<div class="dialog-title">'+this.title+'</div>'+
+					'<div class="dialog-content">'+message+'</div>'+
+					'<div class="dialog-btns">'+
+						(this.onlyConfirm ? 
+							'<button>'+this.confirmText+'</button>':
+							'<button>'+this.cancelText+'</button><button>'+this.confirmText+'</button>')+
+					'</div>'+
+				'</div>';
+	container.innerHTML = html;
+	this.container = container;
+	document.body.appendChild(container);
 }
