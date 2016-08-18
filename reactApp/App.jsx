8870f69087db4ced0ae4e1189b82c5cdd4b29f92
ouyangli <278500368@qq.com>
2016-08-18 16:45:45 +0800

    update component

--- reactApp/App.jsx
+++ reactApp/App.jsx
@@ -3,9 +3,34 @@ import React from 'react';
 class App extends React.Component {
    render() {
       return (
-        <div>
-            Hello World!!!
-        </div>
+         <div>
+            <Header/>
+            <Content/>
+         </div>
+      );
+   }
+}
+
+class Header extends React.Component {
+   render() {
+  	var title = {
+  		color:'red'
+  	};
+      return (
+         <div>
+            <h1 style={title}>Header</h1>
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2 className="new-title">Content</h2>
+            <p>The content text!!!</p>
+         </div>
       );
    }
 }
