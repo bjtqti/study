1dc076c2aa9e36fd26ff4f2d98e21cf053d416ff
ouyangli <278500368@qq.com>
2016-08-19 17:47:59 +0800

    update component Life Cycle

--- reactApp/App.jsx
+++ reactApp/App.jsx
@@ -3,11 +3,27 @@ import Header from './header.jsx';
 import Content from './content.jsx';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+				data: 0
+			}
+		this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+	};
+	forceUpdateHandler() {
+		var newData = ++this.state.data;
+		this.setState({
+			data:newData
+		})
+		//this.forceUpdate();
+	};
    render() {
       return (
          <div>
             <Header/>
-            <Content/>
+            <Content myNumber = {this.state.data}></Content>
+            <button onClick = {this.forceUpdateHandler}>FORCE UPDATE</button>
          </div>
       );
    }
--- reactApp/content.jsx
+++ reactApp/content.jsx
@@ -1,20 +1,43 @@
 import React,{Component} from 'react';
 
 export default class Content extends Component {
-	constructor() {
-      	super();
-      	this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
-   	};
+	constructor(props) {
+		super(props);
+	};
 
-   	forceUpdateHandler() {
-      this.forceUpdate();
-   	};
+	componentWillMount() {
+		console.log('Component WILL MOUNT!')
+	}
+
+	componentDidMount() {
+		console.log('Component DID MOUNT!')
+	}
+
+	componentWillReceiveProps(newProps) {
+		console.log('Component WILL RECIEVE PROPS!',newProps)
+	}
+
+	shouldComponentUpdate(newProps, newState) {
+		return true;
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		console.log('Component WILL UPDATE!');
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		console.log('Component DID UPDATE!')
+	}
+
+	componentWillUnmount() {
+		console.log('Component WILL UNMOUNT!')
+	}
 
    	render() {
       	return (
         <div>
-            <button onClick = {this.forceUpdateHandler}>FORCE UPDATE</button>
             <h4>Random number: {Math.random()}</h4>
+            <p>{this.props.myNumber}</p>
         </div>);
    	}
 
