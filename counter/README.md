3fdbe2a7d3f842aff26db790d01dfbab7bfd289b
bjtqti <278500368@qq.com>
2017-03-21 10:35:29 +0800

    update scrolloop

--- counter/README.md
+++
@@ -1,7 +0,0 @@
-# 计数器
-
-* 模拟计数器的数字滚动效果
-
-## 演示地址
-
-<http://sandbox.runjs.cn/show/ofa50fjm>
--- scrolloop/scrolloop.js
+++ scrolloop/scrolloop.js
@@ -19,7 +19,7 @@ function loopScrollTop(id, second) {
 	var node = wrap.children[0];
 	var stack = [];
 	var height = getHeight(wrap); //==$(wrap).height();
-	var time = (second || 3) * 1000; // 滚动间隔；
+	var time = (second || 1) * 1000; // 滚动间隔；
 	for(var i =0,n=node.children.length;i<n;i++){
 		stack.push(i);
 	}
@@ -28,6 +28,13 @@ function loopScrollTop(id, second) {
 		var distance = index * height;
 		node.style.transform = 'translate3d(0, -' + distance + 'px, 0)';
 		stack.push(index);
+		if(index>0){
+			node.style['-webkit-transition-duration']='800ms';
+			node.style['transition-duration']='800ms';
+		}else{
+			node.style['-webkit-transition-duration']='0ms';
+			node.style['transition-duration']='0ms';
+		}
 		setTimeout(function() {
 			next();
 		}, time);
